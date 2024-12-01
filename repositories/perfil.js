234b871c812649857a792908e4e2d2c35eaddcc0
Harrison Fabiano de Lima <harrisonlima92@gmail.com>
2024-12-01 12:30:57 -0300

    Bugfix Back-end

--- repositories/perfil.js
+++ repositories/perfil.js
@@ -2,7 +2,7 @@ const db = require("../database");
 
 const getPerfils = async (res) => {
   try {
-    const query = `SELECT usuario_perfil.id, usuarios.nome AS "Nome", usuario_perfil.foto AS "Foto", usuario_perfil.area_atuacao AS "Area de atuação", usuario_perfil.biografia AS "Biografia", usuario_perfil.linkedin AS "Linkedin", usuario_perfil.behance AS "Behance", usuario_perfil.figma AS "Figma", usuario_perfil.discord AS "Discord", usuario_perfil.github AS "Github", usuario_perfil.skills AS "Skills" FROM public.usuario_perfil JOIN public.usuarios ON usuario_perfil.usuario_id = usuarios.id ORDER BY nome ASC;`;
+    const query = `SELECT usuario_perfil.id, usuario_perfil.usuario_id, usuarios.nome AS "Nome", usuario_perfil.foto AS "Foto", usuario_perfil.area_atuacao AS "Área de atuação", usuario_perfil.biografia AS "Biografia", usuario_perfil.linkedin AS "Linkedin", usuario_perfil.behance AS "Behance", usuario_perfil.figma AS "Figma", usuario_perfil.discord AS "Discord", usuario_perfil.github AS "Github", usuario_perfil.skills AS "Skills" FROM public.usuario_perfil JOIN public.usuarios ON usuario_perfil.usuario_id = usuarios.id ORDER BY nome ASC;`;
     const { rows } = await db.query(query);
 
     return rows;
@@ -15,7 +15,7 @@ const getPerfils = async (res) => {
 
 const selectPerfil = async (id, res) => {
   try {
-    const query = `SELECT usuario_perfil.id, usuarios.nome AS "Nome", usuario_perfil.foto AS "Foto", usuario_perfil.area_atuacao AS "Area de atuação", usuario_perfil.biografia AS "Biografia", usuario_perfil.linkedin AS "Linkedin", usuario_perfil.behance AS "Behance", usuario_perfil.figma AS "Figma", usuario_perfil.discord AS "Discord", usuario_perfil.github AS "Github", usuario_perfil.skills AS "Skills" FROM public.usuario_perfil JOIN public.usuarios ON usuario_perfil.usuario_id = usuarios.id WHERE usuario_id = $1 ORDER BY nome ASC;`;
+    const query = `SELECT usuario_perfil.id, usuario_perfil.usuario_id, usuarios.nome AS "Nome", usuario_perfil.foto AS "Foto", usuario_perfil.area_atuacao AS "Área de atuação", usuario_perfil.biografia AS "Biografia", usuario_perfil.linkedin AS "Linkedin", usuario_perfil.behance AS "Behance", usuario_perfil.figma AS "Figma", usuario_perfil.discord AS "Discord", usuario_perfil.github AS "Github", usuario_perfil.skills AS "Skills" FROM public.usuario_perfil JOIN public.usuarios ON usuario_perfil.usuario_id = usuarios.id WHERE usuario_id = $1 ORDER BY nome ASC;`;
     const values = [id];
     const { rows } = await db.query(query, values);
     return rows[0];
--- repositories/projeto.js
+++ repositories/projeto.js
@@ -47,11 +47,11 @@ const getProjetos = async (res) => {
 
 const selectProjeto = async (id, res) => {
   try {
-    const query = `SELECT usuario_projetos.id, usuarios.nome AS "Nome", usuario_projetos.titulo AS "Titulo", usuario_projetos.foto_capa AS "Foto Capa", usuario_projetos.hospedagem AS "Hospedagem", usuario_projetos.prototipo AS "Protótipo", usuario_projetos.design AS "Design", usuario_projetos.aplicacao AS "Aplicação", usuario_projetos.descricao AS "Descrição", usuario_projetos.tecnologias AS "Tecnologias" FROM public.usuario_projetos JOIN public.usuarios ON usuario_projetos.usuario_id = usuarios.id WHERE id = $1 ORDER BY nome ASC;`;
+    const query = `SELECT usuario_projetos.id, usuarios.nome AS "Nome", usuario_projetos.titulo AS "Titulo", usuario_projetos.foto_capa AS "Foto Capa", usuario_projetos.hospedagem AS "Hospedagem", usuario_projetos.prototipo AS "Protótipo", usuario_projetos.design AS "Design", usuario_projetos.aplicacao AS "Aplicação", usuario_projetos.descricao AS "Descrição", usuario_projetos.tecnologias AS "Tecnologias" FROM public.usuario_projetos JOIN public.usuarios ON usuario_projetos.usuario_id = usuarios.id WHERE usuario_id = $1 ORDER BY nome ASC;`;
     const values = [id];
     const { rows } = await db.query(query, values);
 
-    return rows[0];
+    return rows;
   } catch (error) {
     console.error("Error: ", error);
     res.status(500).json({ error });
@@ -62,7 +62,6 @@ const selectProjeto = async (id, res) => {
 const updateProjeto = async (id, req, res) => {
   const projeto = req;
   let query, values, item;
-  console.log(projeto)
 
   try {
     query = `UPDATE public.usuario_projetos SET titulo = $1, foto_capa = $2, hospedagem = $3, prototipo = $4, design = $5, aplicacao = $6, descricao = $7, tecnologias = $8 WHERE id = $9 RETURNING *`;
